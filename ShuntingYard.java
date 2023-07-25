fbe82b9db463a4e73463199a205e59bb49cbff97
Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2023-07-26 02:51:35 +0900

    Update ShuntingYard.java

--- ShuntingYard.java
+++ ShuntingYard.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
-class RPN {
+class ShuntingYard {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
